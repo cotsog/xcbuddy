ae22986235b4da551f7eabaf3dc570b9a6f0b6ad
Dominic Jodoin <dominic@travis-ci.com>
2018-04-27 23:25:13 -0400

    Remove dependencies

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "xcbuddy",
-    dependencies: [
-        .package(url: "git@github.com:xcodeswift/xcproj.git", .upToNextMinor(from: "4.2.0")),
-        .package(url: "git@github.com:apple/swift-package-manager.git", .revision("26ab3997142ef464090c63f963d1dafef79a7c06")),
-    ],
     targets: [
         .target(
             name: "ProjectDescription",
@@ -15,10 +11,6 @@ let package = Package(
         .testTarget(
             name: "ProjectDescriptionTests",
             dependencies: ["ProjectDescription"]
-        ),
-        .target(
-            name: "Dependencies",
-            dependencies: ["xcproj", "Utility"]
         ),
     ]
 )
